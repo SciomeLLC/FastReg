fd59e017cd30d1fcb6b316b223f8fe74406735c3
Deepak Mav <deepak.mav@sciome.com>
2022-12-02 12:40:58 -0500

    Minor Changes to DESCRIPTION file
    Former-commit-id: ae386956169dc7a95d1e627fcd922ae647328647

--- tests/test.R
+++ tests/test.R
@@ -1,17 +1,8 @@
-library("hdf5r");
-#library("flexiblas");
 library("data.table");
+library("hdf5r");
+library("FastReg");
 
+setwd("C:/Users/deepak.mav/Documents/data/Projects/NIEHS_Biostat/Alison-Motsinger/FastReg/Packages/data");
 
-setwd("C:/Users/deepak.mav/Documents/data/Projects/NIEHS_Biostat/Alison-Motsinger/FastReg/Packages/FastReg/R");
-source("Array_Functions.R");
-source("logistic.R");
-source("linear.R")
-source("util-functions.R");
-source("parserFunctions.R");
-source("main.R")
-
-setwd("C:/Users/deepak.mav/Documents/data/Projects/NIEHS_Biostat/Alison-Motsinger/FastReg/Packages/FastReg/data");
-
-num.out <- FastReg(config.file="example.num.config")
+num.out <- FastReg(config.file="C:/Users/deepak.mav/Documents/data/Projects/NIEHS_Biostat/Alison-Motsinger/FastReg/Packages/data/example.num.config")
 #bin.out <- FastReg(config.file="example.bin.config")
