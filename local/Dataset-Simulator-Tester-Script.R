bf140c8bd5bf508db271887f1b6c7ac3c6b74808
Shail Choksi <shail.choksi@sciome.com>
2023-06-05 10:54:03 -0400

    Make hdf5 reading robust to handle different compression levels as well as transposed dimensions
    Former-commit-id: 63191b8d2feb7d60727c3156aac056f0d2eeee78

--- local/Dataset-Simulator-Tester-Script.R
+++ local/Dataset-Simulator-Tester-Script.R
@@ -1,14 +1,14 @@
 source("simulate_test_data_function.R");
 
-simulate_test_dataset(num.poi = 50000,
+simulate_test_dataset(num.poi = 5000,
                                   num.ind = 5000,
                                   seed = 12133,
                                   coeff.sd = 0,
                                   bin.resp.mean = 0.2,
                                   num.resp.mean = 24,
                                   num.resp.sd = 5,
-                                  poi.type = "genotypes",
+                                  poi.type = "additive",
                                   poi.chunk.size = 1000,
                                   poi.compression.level=7,
-                                  data.dir = "c:/users/deepak.mav/Documents/data/temp",
-                                  prefix = "testdata_5k_by_50k", verbose=TRUE)
+                                  data.dir = "F:/",
+                                  prefix = "testdata_5000_x_5000_new", verbose=TRUE)
