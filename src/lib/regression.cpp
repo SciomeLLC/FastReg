6631929612a0e8a58e707b105e7169018d284cd8
Shail Choksi <shail.choksi@sciome.com>
2024-10-07 13:07:58 -0400

    Set lower.tail to false

--- src/lib/regression.cpp
+++ src/lib/regression.cpp
@@ -13,7 +13,7 @@ template <typename T> Rcpp::NumericVector arma2vec(const T &x) {
 arma::fcolvec t_dist_r(arma::fcolvec abs_z, int df) {
   arma::fcolvec ret_val(abs_z.size());
   for (size_t i = 0; i < abs_z.size(); i++) {
-    ret_val(i) = log10(2 * R::pt(-1 * abs_z(i), df, true, false));
+    ret_val(i) = log10(2 * R::pt(-1 * abs_z(i), df, false, false));
   }
   return ret_val;
 }
